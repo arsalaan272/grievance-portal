14e3a87038c96800cd7b9662673ae2ebb476af2b
arsalaan272 <arsalaan272158@gmail.com>
2026-07-21 14:41:38 +0530

    added server

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: 'https://grievanceflowai.vercel.app/',
+  origin: 'https://grievanceflowai.vercel.app',
   credentials: true
 }));
 
